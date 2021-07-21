3f9a81f161f7c62da594f9412c5a1753d629e490
hamzaiqbal2000 <hiqbal.bscs18seecs@seecs.edu.pk>
2021-07-21 12:51:27 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//import styles
+import './styles/app.scss'
+//adding components
+import Player from './components/Player';
+import Song from './components/Song';
+
+function App() {
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
+}
+
+export default App;
