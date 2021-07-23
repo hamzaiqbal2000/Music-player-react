009a6bc1644a0d654ca3cdfa96f4044198ba9cb3
hamzaiqbal2000 <hiqbal.bscs18seecs@seecs.edu.pk>
2021-07-23 17:44:34 +0500

    Added some new features

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 //import styles
 import './styles/app.scss'
 //adding components
 import Player from './components/Player';
 import Song from './components/Song';
+//import util
+import data from './util';
 
 function App() {
+  //state
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false)
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player setIsPlaying={setIsPlaying} isPlaying={isPlaying} currentSong={currentSong}/>
     </div>
   );
 }
